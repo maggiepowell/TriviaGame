4417008b32128d4aecac9b351fd1ebe555f07016
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2018-12-28 13:31:39 -0800

    restarted js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,24 @@
-var time
-var window
+//on page load have start button visible
 
-window.onload = function() {
-    $("#start").on("click", start);
-  };
-  
-  function start() {
-    time = 0;
- 
-  }
+//once start button is clicked have times for 20 seconds and question appear with three clickable answer choices
+$("#start").click( function(){
+  var counter = 20;
+  setInterval(function() {
+    counter--;
+     if (counter >= 0) {
+        span = document.getElementById("timer");
+        span.innerHTML = counter;
+     }
+     if (counter === 0) {
+        alert('Sorry, out of time!');
+        clearInterval(counter);
+      }
+    }, 1000);
+});
+//if correct answer is clicked alert "correct!" plus gif for three seconds before next question appears
 
-  function count() {
+//if incorrect answer is clicked alert "wrong!" plus gif for three seconds before next question appears
 
-    time--;
-  }
+//if time expires alert "time's up" plus gif for three seconds before next question appears
+
+//once final question has been answered alert with score for three seconds before returning to start button
